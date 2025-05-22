28f87ae14caf19da97285254263409df84cf99c4
SO Tech <sotech@SOs-MacBook-Pro.local>
2025-05-23 01:37:44 +0530

    auth middleware chnaged

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,18 +1,23 @@
 const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
+  // Prioritize cookie over header for access token
   const token =
     req.cookies.accessToken || req.headers.authorization?.split(" ")[1];
-  console.log("token", token);
-  if (!token)
+
+  if (!token) {
     return res.status(401).json({ message: "Access Denied: No token" });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // id and role
+    req.user = decoded; // Makes req.user available to controllers
     next();
   } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
+    if (err.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "jwt expired" }); // important for auto-refresh logic
+    }
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
 
