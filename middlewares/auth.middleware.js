62a6031d9bad836d0371e79a329e32ef68d999d5
SO Tech <sotech@SOs-MacBook-Pro.local>
2025-06-13 16:25:56 +0530

    console log added in auth middle ware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -5,6 +5,9 @@ const authenticate = (req, res, next) => {
   const token =
     req.cookies?.accessToken || req.headers.authorization?.split(" ")[1];
 
+  console.log("AccessToken:", req.cookies?.accessToken);
+  console.log("Headers:", req.headers);
+
   if (!token) {
     return res.status(401).json({ message: "Access Denied: No token" });
   }
